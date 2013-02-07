306d91aec4ff83c5fe32a3b1481c2b4bb72df1e8
Christian A. Rodriguez <christian.etpr10@gmail.com>
2013-02-06 21:42:53 -0400

    Updated README

--- randomQuotes/randomQuotes.js
+++ randomQuotes/randomQuotes.js
@@ -7,7 +7,7 @@ var express = require('express'),
 if (process.env.REDISTOGO_URL) {
 var rtg   = require("url").parse(process.env.REDISTOGO_URL);
 var redis = require("redis").createClient(rtg.port, rtg.hostname);
-
+console.log("Connected to redis server");
 redis.auth(rtg.auth.split(":")[1]); 
 } else {
 var redis = require("redis");
